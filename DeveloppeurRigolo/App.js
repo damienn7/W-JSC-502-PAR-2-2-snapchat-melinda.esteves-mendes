7e9d1d7f1cdf60b2b00f38303e92bd1142170e9e
Melinda-EM <melinda.esteves-mendes@epitech.eu>
2023-06-22 16:00:53 +0200

    [ADD] Camera

--- DeveloppeurRigolo/App.js
+++ DeveloppeurRigolo/App.js
@@ -1,63 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View, Image } from 'react-native';
 
-import Button from './components/Button';
-import Logo from './components/Logo';
+// import Button from './app/components/Button';
+// import Logo from './app/components/Logo';
 
-const Images = require('./assets/snapchat.png');
+// const Images = require('./assets/snapchat.png');
 
-const array = ['#FFD700', '#FFEE75', '#b0f2b6', '#8F00FF' ];
-const randomElement = array[Math.floor(Math.random() * array.length)];
+// const array = ['#FFD700', '#FFEE75', '#b0f2b6', '#8F00FF' ];
+// const randomElement = array[Math.floor(Math.random() * array.length)];
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.imageContainer}>
-        <Logo placeholderImageSource={Images} />
-      </View>
-      <Text style={styles.text}>Bonjour jeune Papillon ! {'\n'}Rejoins l'essaim dès maintenant</Text>
-      <View style={styles.buttonContainer}>
-        <Button
-          label="Inscription"
-          onPress={() => console.log('Bouton Inscription pressé')}
-          buttonStyle={styles.buttonInscription}
-        />
-        <Button
-          label="Connexion"
-          onPress={() => console.log('Bouton Connexion pressé')}
-          buttonStyle={styles.buttonConnexion}
-        />
-      </View>
-      <StatusBar style="auto" />
-    </View>
-  );
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <View style={styles.imageContainer}>
+//         <Logo placeholderImageSource={Images} />
+//       </View>
+//       <Text style={styles.text}>Bonjour jeune Papillon ! {'\n'}Rejoins l'essaim dès maintenant</Text>
+//       <View style={styles.buttonContainer}>
+//         <Button
+//           label="Inscription"
+//           onPress={() => console.log('Bouton Inscription pressé')}
+//           buttonStyle={styles.buttonInscription}
+//         />
+//         <Button
+//           label="Connexion"
+//           onPress={() => console.log('Bouton Connexion pressé')}
+//           buttonStyle={styles.buttonConnexion}
+//         />
+//       </View>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: randomElement,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     paddingBottom: 150,
+//   },
+//   text: {
+//     color: 'black',
+//     fontWeight: 'bold',
+//     paddingBottom: 50,
+//   },
+//   imageContainer: {
+//     flex: 1,
+//     paddingTop: 30,
+//   },
+//   buttonContainer: {
+//     marginTop: 30,
+//   },
+//   buttonInscription: {
+//     backgroundColor: '#DA012D', 
+//     marginBottom: 5,
+//   },
+//   buttonConnexion: {
+//     backgroundColor: '#007FFF',
+//   },
+// });
+
+import React from "react";
+import CameraScreen from "./app/screens/CameraScreen";
+
+const App = () => {
+  return(
+    <CameraScreen />
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: randomElement,
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingBottom: 150,
-  },
-  text: {
-    color: 'black',
-    fontWeight: 'bold',
-    paddingBottom: 50,
-  },
-  imageContainer: {
-    flex: 1,
-    paddingTop: 30,
-  },
-  buttonContainer: {
-    marginTop: 30,
-  },
-  buttonInscription: {
-    backgroundColor: '#DA012D', 
-    marginBottom: 5,
-  },
-  buttonConnexion: {
-    backgroundColor: '#007FFF',
-  },
-});
+export default App;
